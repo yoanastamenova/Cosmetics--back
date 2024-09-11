357740c45b2837d012a64a979eb54e0bdd4d8549
More-Pe <peraltamorena.va@gmail.com>
2024-09-11 10:46:31 +0200

    feat: added Admin middleware

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        {
+            if($request->is_admin === 'true')
+            {
+                return $next($request);
+            }
+            return response()->json('No estás autorizado', 401);
+        }
+    }
+}
